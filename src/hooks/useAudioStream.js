06f1072aa94759c21233045e5d5bd57a222bb2a2
Jana E. Beck <jana.eliz.beck@gmail.com>
2020-04-30 18:24:28 -0700

    just adding a TODO comment...

--- src/hooks/useAudioStream.js
+++ src/hooks/useAudioStream.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 
+/** TODO: component that calls navigator.mediaDevices.enumerateDevices() and renders dropdown/selection */
 export function useAudioStream({ audioCtx, destination }) {
   useEffect(() => {
     const target = destination || audioCtx.destination;
